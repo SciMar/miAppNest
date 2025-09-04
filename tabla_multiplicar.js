452983f218ab13729f2b3a3e5ce032b2df614cbb
SciMar <cramirezanzola@gmail.com>
2025-09-04 13:40:32 -0500

    Tabla del 1 a l 10

--- tabla_multiplicar.js
+++ tabla_multiplicar.js
@@ -1,10 +1,23 @@
-let base = 7;
+/*let base = 7;
 let indice = 1;
 
 while (indice <=10){
     console.log(`${base} x ${indice} = ${base*indice}`);
     indice++;
-}
+}*/
 
 
 //TAREA: TALAS DEL 1 AL 10
+
+let numero = 1;
+
+while (numero<=10){
+    console.log(`\nTabla del ${numero}`);
+
+    let numeroDos = 1;
+    while (numeroDos<=10){
+        console.log (`${numero} x ${numeroDos}=${numero * numeroDos}`);
+        numeroDos++;
+    }
+    numero ++
+}
