c295e20ebc7aea12dc0fc9bf00761d06156183ca
SciMar <cramirezanzola@gmail.com>
2025-10-13 00:06:47 -0500

    [FEAT] conexión a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,34 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'process';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}), UsersModule, ProductsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}), 
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true, //para que cargue las entidades automaticamente
+        synchronize: false, //false en produccion, true en desarrollo, para que sincronice los cambios en las entidades con la base de datos
+      })
+    }),
+    UsersModule, 
+    ProductsModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
